0cb372d2f04f661c87229410300962594fbe991f
Praveenkumar <praveenitmec@gmail.com>
2017-08-04 20:07:13 +0530

    Added category and Some Css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -72,6 +72,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title, :description)
+      params.require(:question).permit(:title, :description, :category_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802140450) do
+ActiveRecord::Schema.define(version: 20170804124935) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -22,12 +22,20 @@ ActiveRecord::Schema.define(version: 20170802140450) do
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_questions_on_category_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
